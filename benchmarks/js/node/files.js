e22b4bd9876abede51eb26100a1855584e15803f
Piotr Tomczak <piotr.t@niceguys.info>
2024-01-19 06:36:43 +0100

    feat(files): in progress

--- benchmarks/js/node/files.js
+++ benchmarks/js/node/files.js
@@ -3,47 +3,44 @@ const path = require("path");
 const { faker, en } = require("@faker-js/faker");
 
 const createFile = async (fileName, numberOfParagraphs) => {
-	return await fs.writeFile(fileName, faker.lorem.paragraphs(numberOfParagraphs), (err) => console.error(err));
+    return await fs.writeFile(fileName, faker.lorem.paragraphs(numberOfParagraphs), (err) => console.error(err));
 };
 
-const createBatchOfFiles = async (numberOfFiles, startFileName, numberOfParagraphs = 20, directory = "tmp") => {
-	const fileNames = [];
-	for (let i = 0; i < numberOfFiles; i++) {
-		if (!fs.existsSync(path.join(__dirname, directory))) {
-			await fs.mkdir(path.join(__dirname, directory), (err) => console.error(err));
-		}
-		const fileName = path.join(__dirname, directory, `${startFileName}-${i + 1}.txt`);
-		await createFile(fileName, numberOfParagraphs);
-		fileNames.push(fileName);
-	}
-	return fileNames;
+const createBatchOfFiles = async (numberOfFiles, startFileName, numberOfParagraphs = 20) => {
+    const fileNames = [];
+    for (let i = 0; i < numberOfFiles; i++) {
+        const fileName = path.join(__dirname, `${startFileName}-${i + 1}.txt`).toString();
+        await createFile(fileName, numberOfParagraphs);
+        fileNames.push(fileName);
+    }
+    return fileNames;
 };
 
-const parseContent = async (data, result, startTime) => {
-	const endTime = performance.now();
-	result.push({ data: data.toString(), time: endTime - startTime });
-};
+const readFiles = (fileNames) => {
+    const resultOfWriting = [];
+    const startTime = performance.now();
 
-const readFiles = async (fileNames) => {
-	const resultOfWriting = [];
-	const startTime = performance.now();
+    for (const fileName of fileNames) {
+        try {
+            const textFileName = fileName
+                .split("/")
+                .filter((p) => p)
+                .reverse()[0];
+            const lines = fs.readFileSync(path.join(__dirname, `tmp/${textFileName}`));
+            const content = lines.toString();
+            console.log("🚀 ~ readFiles ~ content:", content);
+            console.log(lines);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    const endTime = performance.now();
 
-	for (const fileName of fileNames) {
-		const startTime = performance.now();
-		try {
-			const lines = fs.readFile(fileName, (err, data) => !err && parseContent(data, resultOfWriting, startTime));
-			console.log(lines);
-		} catch (err) {
-			console.error(err);
-		}
-	}
-	const endTime = performance.now();
-
-	return { results: resultOfWriting, time: endTime - startTime };
+    return { results: resultOfWriting, time: endTime - startTime };
 };
 
 (async () => {
-	const fileNames = await createBatchOfFiles(10, "lorem", 100);
-	const result = await readFiles(fileNames);
-	console.log(result);
+    const fileNames = await createBatchOfFiles(10, "lorem", 100);
+    const result = readFiles(fileNames);
+    console.log(result);
 })();
