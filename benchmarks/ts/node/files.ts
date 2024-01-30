399497a84c0d9086e7e38cf8732b0e5abbff3a73
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2024-01-30 19:52:53 +0100

    feat(Bun): Create files using Bun.file

---
+++ benchmarks/ts/node/files.ts
@@ -0,0 +1,63 @@
+import fs from "fs";
+import path from "path";
+import { faker } from "@faker-js/faker";
+
+const createFile = (fileName: string, numberOfParagraphs: number) => {
+	return fs.writeFileSync(fileName, faker.lorem.paragraphs(numberOfParagraphs));
+};
+
+const createBatchOfFiles = (
+	numberOfFiles: number,
+	startFileName: string,
+	numberOfParagraphs = 20,
+	directory = "tmp"
+) => {
+	const startTime = performance.now();
+	const fileNames = [];
+	if (!fs.existsSync(path.join(__dirname, directory).toString())) {
+		fs.mkdirSync(path.join(__dirname, directory));
+	}
+	for (let i = 0; i < numberOfFiles; i++) {
+		const fileName = path.join(__dirname, directory, `${startFileName}-${i + 1}.txt`).toString();
+		createFile(fileName, numberOfParagraphs);
+		fileNames.push(fileName);
+	}
+	const endTime = performance.now();
+	return { fileNames: fileNames, time: endTime - startTime };
+};
+
+const readFiles = (fileNames: string[]) => {
+	const resultOfWriting = [];
+	const startTime = performance.now();
+
+	for (const fileName of fileNames) {
+		try {
+			const realFileName = fileName
+				.split("/")
+				.filter((s) => s)
+				.reverse()[0];
+			const lines = fs.readFileSync(path.join(__dirname, "tmp", realFileName).toString(), { encoding: "utf-8" });
+			const content = lines.toString();
+			resultOfWriting.push(content);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+	const endTime = performance.now();
+
+	return { results: resultOfWriting, time: endTime - startTime };
+};
+
+const removeFiles = (directory = "tmp") => {
+	fs.rmSync(path.join(__dirname, directory), { recursive: true, force: true });
+};
+
+(() => {
+	const startTime = performance.now();
+	const fileNames = createBatchOfFiles(10, "lorem", 100);
+	const result = readFiles(fileNames.fileNames);
+	const endTime = performance.now();
+	console.log(endTime - startTime);
+	console.log("🚀 ~ result:", result);
+	removeFiles();
+})();
