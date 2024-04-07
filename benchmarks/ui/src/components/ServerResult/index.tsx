5d2b590045ba1861e794e76e5394b7f8f0edacd3
amelinium23 <piotr.tomczak.praca@gmail.com>
2024-04-07 15:38:41 +0200

    feat: server results done

--- benchmarks/ui/src/components/ServerResult/index.tsx
+++ benchmarks/ui/src/components/ServerResult/index.tsx
@@ -1,5 +1,11 @@
 import { useGetServerResultsQuery } from "@/store/services/benchmarkService";
 
+import jsBunResult from "../../../../js/bun/bunServerResult.json";
+import jsDenoResult from "../../../../js/deno/denoServerResult.json";
+import jsNodeResult from "../../../../js/node/nodeServerResult.json";
+import tsBunResult from "../../../../ts/bun/bunServerResult.json";
+import tsDenoResult from "../../../../ts/deno/denoServerResult.json";
+import tsNodeResult from "../../../../ts/node/nodeServerResult.json";
 import { Card, CardContent, CardTitle } from "../ui/card";
 import { Skeleton } from "../ui/skeleton";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
@@ -13,6 +19,33 @@ type SummaryKeys =
   | "details"
   | "statusCodeDistribution"
   | "errorDistribution";
+
+const jsonData = {
+  js: {
+    result_bun: jsBunResult,
+    result_deno: jsDenoResult,
+    result_node: jsNodeResult
+  },
+  ts: {
+    result_bun: tsBunResult,
+    result_deno: tsDenoResult,
+    result_node: tsNodeResult
+  }
+};
+
+const showValue = (value: unknown) => {
+  if (typeof value === "string" || typeof value === "number") {
+    return value;
+  }
+  if (typeof value === "object" && value) {
+    return Object.entries(value).map(([key, value]) => (
+      <TableRow key={key}>
+        <TableCell>{value as string | number}</TableCell>
+      </TableRow>
+    ));
+  }
+  return null;
+};
 
 export const ServerResult = ({ req }: ServerResultProps) => {
   const { data, isLoading } = useGetServerResultsQuery(req);
@@ -48,7 +81,99 @@ export const ServerResult = ({ req }: ServerResultProps) => {
                               ([insertedKey, value]) => (
                                 <TableRow key={insertedKey}>
                                   <TableCell>{insertedKey}</TableCell>
-                                  <TableCell>{}</TableCell>
+                                  <TableCell>{showValue(value)}</TableCell>
+                                </TableRow>
+                              )
+                            )
+                          : null}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </>
+              ))}
+              {Object.keys(data.ts).map((key) => (
+                <>
+                  <TableCaption className="flex text-center w-full">
+                    {key
+                      .split("_")
+                      .filter((x) => x)
+                      .join(" - ts")}
+                  </TableCaption>
+                  {Object.keys(data.ts[key as ResultKeys]).map((summaryKey) => (
+                    <TableRow key={summaryKey}>
+                      <TableCell>{summaryKey}</TableCell>
+                      <TableCell className="break-words max-w-[300px]">
+                        {typeof data.ts[key as ResultKeys][summaryKey as SummaryKeys] === "object"
+                          ? Object.entries(data.ts[key as ResultKeys][summaryKey as SummaryKeys]).map(
+                              ([insertedKey, value]) => (
+                                <TableRow key={insertedKey}>
+                                  <TableCell>{insertedKey}</TableCell>
+                                  <TableCell>{showValue(value)}</TableCell>
+                                </TableRow>
+                              )
+                            )
+                          : null}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </>
+              ))}
+            </TableBody>
+          </Table>
+        ) : jsonData ? (
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead>Result</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Object.keys(jsonData.js).map((key) => (
+                <>
+                  <TableCaption className="flex text-center w-full">
+                    {key
+                      .split("_")
+                      .filter((x) => x)
+                      .join(" - js")}
+                  </TableCaption>
+                  {Object.keys(jsonData.js[key as ResultKeys]).map((summaryKey) => (
+                    <TableRow key={summaryKey}>
+                      <TableCell>{summaryKey}</TableCell>
+                      <TableCell className="break-words max-w-[300px]">
+                        {typeof jsonData.js[key as ResultKeys][summaryKey as SummaryKeys] === "object"
+                          ? Object.entries(jsonData.js[key as ResultKeys][summaryKey as SummaryKeys]).map(
+                              ([insertedKey, value]) => (
+                                <TableRow key={insertedKey}>
+                                  <TableCell>{insertedKey}</TableCell>
+                                  <TableCell>{showValue(value)}</TableCell>
+                                </TableRow>
+                              )
+                            )
+                          : null}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </>
+              ))}
+              {Object.keys(jsonData.ts).map((key) => (
+                <>
+                  <TableCaption className="flex text-center w-full">
+                    {key
+                      .split("_")
+                      .filter((x) => x)
+                      .join(" - ts")}
+                  </TableCaption>
+                  {Object.keys(jsonData.ts[key as ResultKeys]).map((summaryKey) => (
+                    <TableRow key={summaryKey}>
+                      <TableCell>{summaryKey}</TableCell>
+                      <TableCell className="break-words max-w-[300px]">
+                        {typeof jsonData.ts[key as ResultKeys][summaryKey as SummaryKeys] === "object"
+                          ? Object.entries(jsonData.ts[key as ResultKeys][summaryKey as SummaryKeys]).map(
+                              ([insertedKey, value]) => (
+                                <TableRow key={insertedKey}>
+                                  <TableCell>{insertedKey}</TableCell>
+                                  <TableCell>{showValue(value)}</TableCell>
                                 </TableRow>
                               )
                             )
